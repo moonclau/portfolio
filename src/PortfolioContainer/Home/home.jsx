fb40d6b1e0b9dafaa4db05866a3f4c3b88140a70
moonclau <ccruzh@toluca.tecnm.mx>
2024-09-05 23:45:12 -0600

    change main section

--- src/PortfolioContainer/Home/home.jsx
+++ src/PortfolioContainer/Home/home.jsx
@@ -5,34 +5,24 @@ import { FaLinkedin } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { FaGithubSquare } from "react-icons/fa";
 import { TbFileCv } from "react-icons/tb";
+import { MdOutlineWavingHand } from "react-icons/md";
 
 function home() {
   return (
-    <div className="w-full grid grid-cols-3 grid-rows-2 gap-4 ">
-      <div className="w-full col-span-2 info-profile">
-        <div className="w-full ">
-          <div className="w-full ">
-            <h1 className="text-5xl flex justify-center items-center mt-44  text-white">Hola, soy Clau</h1>
-            <h2 className="text-3xl flex justify-center items-center mt-5  ml-12  text-white">Desarrollador FullStack</h2>
-          </div>
-          <div className="w-full h-5 flex justify-center items-center">
-            <div className="w-3/12 grid grid-cols-4 pt-5 mt-5  ml-20 grid-rows-1 ">
-              <div><FaGithubSquare className="text-white text-4xl"/></div>
-              <div><FaLinkedin className="text-white text-4xl"/></div>
-              <div><IoMdMail className="text-white text-4xl"/></div>
-              <div><TbFileCv className="text-white text-4xl"/></div>
-            </div>
-          </div>
-            <div className="w-full  flex justify-center items-center mt-10 ml-8 pt-5">
-                <button className="w-44 bg-[#fb8500] ml-5 p-3 rounded-3xl text-white focus:shadow-outline hover:bg-[#ffb703]">Contrátame</button>
-                </div>
+    <div className="w-full grid grid-cols-2 grid-rows-2 gap-4 bg-[#fff6e7]">
+      <div className="w-full flex items-center justify-center justify-self-center text-center">
           
-          <img alt="sunflower" className="w-16 mt-10 ml-20" src={sunFlower} />
+        <div className="grid grid-rows-1 ">
+          <div className="grid grid-cols-2 place-self-center">
+            <p className="text-3xl mr-1">Hola, soy Clau </p>
+            <MdOutlineWavingHand className="size-8"/>
+          </div>
+          <h1 className="text-5xl	text-orange-500">Desarrollador Web</h1>
 
+            <div className="text-lg">Soy desarrollador fullstack con experiencia en la creación <br/>y mantenimiento de aplicaciones web.</div>  
         </div>
-
       </div>
-      <div className="flex justify-center items-center col-start-3 mr-2 pr-10">
+      <div className="flex justify-center items-center  mr-2 pr-10">
         <img alt="imagen logo" className="" src={perfilFoto} />
       </div>
     </div>
--- src/PortfolioContainer/Templates/navbar/NavbarStruct.jsx
+++ src/PortfolioContainer/Templates/navbar/NavbarStruct.jsx
@@ -2,17 +2,18 @@ import Logo from "../../../../public/images/logo.svg";
 import { NavLink } from "react-router-dom";
 import './NavbarStruct.css';
 const NavbarStruct = () => {
+  const content = (<div></div>); 
   return (
-    <div className="navbar w-full mb-0">
-      <nav >
+    <div className="navbar w-full mb-0 ">
+      <nav className="bg-[#fff6e7]">
         <div className="h-10vh flex justify-between flex-1">
           <div className="w-full flex items-center">
             <NavLink to="/">
               <img alt="imagen logo" className="w-6/12" src={Logo} />
             </NavLink>
-            <div className="w-full lg:flex md:flex lg: flex-1 items right justify-end mr-2.5	text-lg">
+            <div className="w-full lg:flex md:flex lg: flex-1 items center justify-center mr-2.5">
               <div className="flex-10 ">
-                <ul className="flex  gap-8 mr-16 text-[18px]">
+                <ul className="flex  gap-8 mr-16 text-3xl	">
                   <NavLink to="/">
                     <li className="py-7">Inicio</li>
                   </NavLink>
@@ -20,7 +21,7 @@ const NavbarStruct = () => {
                     <li className="py-7">Sobre mi</li>
                   </NavLink>
                   <NavLink to="/ContactMe">
-                    <li className="py-7">Contacto</li>
+                    <li className="py-7 ">Contacto</li>
                   </NavLink>
                 </ul>
               </div>
