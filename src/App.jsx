6eb77c4788d6d8826fa858a2358eaf4d5548b747
moonclau <ccruzh@toluca.tecnm.mx>
2024-10-24 07:57:06 -0600

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className='app'>
       <ThemeProvider>
-        <Routes>
+        <Routes basename="/portfolio">
           <Route path="/" element={<Template />}>
           </Route>
         </Routes> 
