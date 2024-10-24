45b524a18f16e18ea3a82d498a33c22e1c98b8fb
moonclau <ccruzh@toluca.tecnm.mx>
2024-10-24 07:23:44 -0600

    change del¡ploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import './App.css'
 import {Routes, Route} from 'react-router-dom';
-// import AboutMe from "./PortfolioContainer/AboutMe/AboutMe.jsx"
-// import CertificatesPage from "./PortfolioContainer/Certificates/Certificates.jsx";
-// import ContactMe from './PortfolioContainer/ContactMe/ContactMe.jsx';
-// import Experience from "./PortfolioContainer/Experience/Experience.jsx";
-// import Projects from "./PortfolioContainer/Projects/Projects.jsx";
-// import Skills from "./PortfolioContainer/Skills/Skills.jsx";
- import Template from "./PortfolioContainer/Templates/template.jsx";
+import Template from "./PortfolioContainer/Templates/template.jsx";
 import { ThemeProvider } from './imports/ThemeContext';
 
 function App() {
@@ -16,12 +10,6 @@ function App() {
       <ThemeProvider>
         <Routes>
           <Route path="/" element={<Template />}>
-            {/* <Route path="/" element={<AboutMe />} />
-            <Route path="/ContactMe" element={<ContactMe />} />
-            <Route path="/Experience" element={<Experience />} />
-            <Route path="/Projects" element={<Projects />} />
-            <Route path="/Skills" element={<Skills />} /> 
-            <Route path="/Projects" element={<CertificatesPage />} /> */}
           </Route>
         </Routes> 
       </ThemeProvider>
